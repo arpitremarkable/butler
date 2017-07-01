133d4e2b3f2ae4dc504f0e58bde899597076ec8c
arpit o.O <arpitremarkable@gmail.com>
2017-07-02 01:53:01 +0530

    Better admin

--- butler/brunch/admin.py
+++ butler/brunch/admin.py
@@ -9,12 +9,12 @@ from brunch.models import ExplorerSource
 class ExplorerSourceAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'link', )
     list_display_links = ('__unicode__', 'link', )
-    list_select_related = ('source', )
-    list_filter = ('source__title',)
-    fields = ('source', )
+    list_select_related = ('query', )
+    list_filter = ('query__title',)
+    fields = ('query', )
 
     def link(self, instance):
-        return '<a href="%s" target="_blank">open</a>' % (instance.source.get_absolute_url(), )
+        return '<a href="%s" target="_blank">open</a>' % (instance.query.get_absolute_url(), )
     link.allow_tags = True
 
 
